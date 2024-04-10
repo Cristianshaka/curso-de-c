ac57edd3878be8f03c930522628e31b37298a224
Cristian Shaka Ferreira Nken <cristian.nken@dinfor.uniceub>
2024-04-10 18:08:35 -0300

    oiii

---
+++ test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+   int abacates;
+   float valor;
+    printf("Quantos abacates?: ");
+    scanf("%d", &abacates);
+    if (abacates <= 5){
+        valor = abacates*1;
+        
+        printf("Valor: %.1f", valor);
+        
+        
+    }else if(abacates >= 6 && abacates <= 10){
+         valor = abacates*0.80;
+        printf("Valor: %.1f", valor);
+        
+    }else if(abacates > 10){
+         valor = abacates*0.60;
+        printf("Valor: %.1f", valor);
+       
+}else{
+    printf("Inválido");
+}
+
+    return 0;
+}
